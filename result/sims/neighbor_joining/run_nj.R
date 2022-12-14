d0bae28bb816b709eb2f5fc9f7caba3a0c079b9e
Vikram Ramavarapu <54862162+vikramr2@users.noreply.github.com>
2022-12-14 13:41:19 -0600

    Delete result directory

--- result/sims/neighbor_joining/run_nj.R
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/Rscript
-suppressMessages(library(phangorn))
-
-args <- commandArgs(trailingOnly = TRUE)
-d <- read.table(args[1], header = T, row.names = "sample")
-mat.nj = nj(dist.gene(d))
-cat(paste0(write.tree(mat.nj), "\n"))
